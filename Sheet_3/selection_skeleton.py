cfce97537c9c7469b654f2a9c789e455e53aacad
gosow9 <cedric.r.sbx@swissonline.ch>
2023-03-28 22:33:39 +0200

    Excercise 1 start

--- Sheet_3/selection_skeleton.py
+++ Sheet_3/selection_skeleton.py
@@ -1,43 +1,63 @@
 import numpy as np
 from math import sqrt
-
-#Firstly we load the data, if you don't have pandas install you can load it as a numpy array:
-signal = np.loadtxt("signal.txt")
-background = np.loadtxt("background.txt")
-
-#However, if you have pandas installed, its much nicer to use pandas DataFrames for easier manipulation
+import matplotlib.pyplot as plt
 import pandas as pd
 
+def ex_1_a(signal, background):
+    length = int(signal.shape[1])
+    for i in range(length):
+        plt.figure()
+        plt.hist(signal[:,i], alpha=0.5, label='Signal', bins=20)
+        plt.hist(background[:,i], alpha=0.5, label='Background', bins=20)
+        plt.xlabel(f'Feature {i+1}')
+        plt.ylabel('Density')
+        plt.legend(loc='upper right')
+        plt.show()
+
+
+def fisher_score(data1, data2):
+    rows, cols = data1.shape
+    fisher = np.zeros((0, cols))
+    mu = np.mean(data1)
+    sig = np.std(data1)
+    means = np.mean(data1, axis=0)
+    stds = np.std(data1, axis=0)
+    print(len(means))
+
+
+def ex_1_b(signal, background):
+    fisher_score(signal)
+
+def ex_1(signal, background):
+    print("Excercise 1.a)")
+    #ex_1_a(signal, background)
+    print("Excercise 1.b)")
+    ex_1_b(signal, background)
+
+"""
 #Here we read the txt file as a pandas 'DataFrame'
-signal = pd.read_csv('signal.txt', sep=" ")
-
-
+signal_df = pd.read_csv('signal.txt', sep=" ")
 #This sets the label of each feature
-columns = ["PT1","PT2","P1","P2","TotalPT","VertexChisq","Isolation"]
-signal.columns = columns 
+columns = ["PT1", "PT2", "P1", "P2", "TotalPT", "VertexChisq", "Isolation"]
+signal_df.columns = columns
+
+background_df = pd.read_csv('background.txt', sep=" ")
+background_df.columns = columns
 
 
-background = pd.read_csv('background.txt', sep=" ")
-background.columns = signal.columns
-
-print(signal)
-print(background)
-
-'''
 #Lets calculate what the accuracy is before any more selection
 metric_init = len(signal)/(len(background)+len(signal))
 
 print('Inital metric value is ',metric_init)
-'''
 
 
-'''
+
 #Now we try a selection, cutting the data for values of the vertex chisq < 4.
 selection_cut = 'VertexChisq < 4'
 
 #Reduce the dataset using the query command (for arrays can use array.select)
-signal_select = signal.query(selection_cut)
-background_select = background.query(selection_cut)
+signal_select = signal_df.query(selection_cut)
+background_select = background_df.query(selection_cut)
 
 #Calculate the signal and background efficiency using the ratio of DataFrame sizes
 signal_efficiency = len(signal_select)*1.0/len(signal)
@@ -56,10 +76,10 @@ FN = len(background)*background_efficiency
 metric = (TP+TN)/(TP+FP+TN+FN)
 
 print('Metric after selection is',metric)
-'''
 
 
-'''
+
+
 #The following code could be used to train a BDT
 
 #We load the input as normal numpy arrays
@@ -100,4 +120,8 @@ y_predicted = bdt.predict(X_test)
 df = pd.DataFrame(np.hstack((X_test, y_predicted.reshape(y_predicted.shape[0], -1),y_test.reshape(y_test.shape[0],-1))),
                   columns=columns+['BDT val','Label'])
 print(df)
-'''
+"""
+if __name__ == "__main__":
+    signal = np.loadtxt("signal.txt")
+    background = np.loadtxt("background.txt")
+    ex_1(signal, background)
